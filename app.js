3ff57344f851fb213dea3c95769397263454a3b8
Allen Chen <alnchen@hotmail.com>
2018-05-27 15:33:56 -0700

    Initial

---
+++ app.js
@@ -0,0 +1,16 @@
+// Sample node.js web app for quick test
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+  });
+});
+
+app.listen(8888);
+module.exports.getApp = app;
